825cfb526e7049757d71cef270586bc53de11a33
John George <68780394+johngeorge88@users.noreply.github.com>
2020-12-07 22:13:14 -0500

    updating the users model

--- server.js
+++ server.js
@@ -7,14 +7,12 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
-
 app.use(require('./routes'));
 
-// Importing mongoose
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/Pizza-hunt', {
-    useFindAndModify: false,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/pizza-hunt', {
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 });
 
 // Use this to log mongo queries being executed!
